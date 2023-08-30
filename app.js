9741f4c6d50e7824b40ddc979d83ed18fcf5d6dd
Fayzulla0755 <qurbonxojayevfayzullaxoja@gmail.com>
2023-08-30 19:03:19 +0500

    mogodb o'zgartirildi

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose')
-const cors = require('cors');
-const port = process.env.PORT || '5000';
+const mongoose = require("mongoose");
+const cors = require("cors");
+const port = process.env.PORT || "5000";
 
-const rProducts = require('./routers/product')
-const rUser = require('./routers/users')
-const rAuth = require('./routers/auth')
-
-
-
+const rProducts = require("./routers/product");
+const rUser = require("./routers/users");
+const rAuth = require("./routers/auth");
 
 // Mongoodb conecting
-mongoose.set('strictQuery', false);
-mongoose.connect('mongodb+srv://qurbonxojayevfayzullaxoja:oqPZDRCSTHNK0T7z@users.nl7eatz.mongodb.net/');
+mongoose.set("strictQuery", false);
+mongoose.connect(
+    "mongodb+srv://qurbonxojayevfayzullaxoja:oqPZDRCSTHNK0T7z@users.nl7eatz.mongodb.net/?retryWrites=true&w=majority"
+);
 const db = mongoose.connection;
-db.on('open', () => console.log("Mongodb ishladi"));
-db.on('error', (err) => console.log(err));
+db.on("open", () => console.log("Mongodb ishladi"));
+db.on("error", (err) => console.log(err));
 
 //Congig
 app.use(cors());
@@ -29,7 +28,4 @@ app.use(rProducts);
 app.use(rUser);
 app.use(rAuth);
 
-
-
-
-app.listen(port, () => console.log(`Server ${port} da ishladi`))   
+app.listen(port, () => console.log(`Server ${port} da ishladi`));
